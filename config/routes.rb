e7dabcd11c6e1704ee9cbaa350171dddd3cb7efa
Phil Alekseev <phil.alekseev@gmail.com>
2015-10-05 14:51:22 -0400

    Added Stories

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :v1, defaults: { format: :json } do
     resource :login, only: [:create], controller: :sessions
     resources :users, only: [:create]
+    resources :stories, only: [:index, :show, :create]
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
